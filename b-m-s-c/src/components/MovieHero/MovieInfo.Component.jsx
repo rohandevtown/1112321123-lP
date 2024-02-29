86502686ed315b0b697a949641e1f749e0a13c07
rohandevtown <rohankinnal@devtown.in>
2024-02-29 19:00:45 +0530

    done

--- b-m-s-c/src/components/MovieHero/MovieInfo.Component.jsx
+++ b-m-s-c/src/components/MovieHero/MovieInfo.Component.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import PaymentModel from '../PaymentModal/Payment.Component';
 
 const MovieInfo = ({movie}) => {
 
@@ -18,7 +19,7 @@ const MovieInfo = ({movie}) => {
     }
 
   return <>
-    {/* <PaymentModel setIsOpen={setIsOpen} isOpen={isOpen} price={price} /> */}
+    <PaymentModel setIsOpen={setIsOpen} isOpen={isOpen} price={price} />
     <div className='flex flex-col gap-8'>
         <h1 className='text-white text-5xl font-bold'>{movie.original_title}</h1>
                     <div className='text-white flex flex-col gap-2 md:px-4'>
@@ -27,10 +28,10 @@ const MovieInfo = ({movie}) => {
                         <h4>{movie.runtime} min | {genres}</h4>
                     </div>
                     <div className='flex items-center gap-3 md:px-4 text-xl '>
-                    <button className='bg-red-500 w-full py-3 text-white font-semibold rounded-lg'>
+                    <button onClick={rentMovie} className='bg-red-500 w-full py-3 text-white font-semibold rounded-lg'>
                         Rent ₹ 149
                     </button>
-                     <button className='bg-red-600 w-full py-3 text-white font-semibold rounded-lg'>
+                     <button onClick={buyMovie} className='bg-red-600 w-full py-3 text-white font-semibold rounded-lg'>
                         Buy ₹ 599
                     </button>
                 </div>
